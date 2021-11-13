7b750894fa13703bae05e318e8dd2f9e6bbb812e
Cyber-Davinci <=>
2021-11-13 08:48:27 -1200

    Updated

--- Package/main.py
+++ Package/main.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('layout.html')
+    return render_template('main.html')
 
+@app.route("/join")
+def join():
+    return render_template('join.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
